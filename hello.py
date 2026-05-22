f26f96800b82876cef4c52732e10b98bb0303a16
Wojciech Żarski <wojciech.zarski@gmail.com>
2026-05-22 20:32:06 +0200

    feat: add hello.py

---
+++ hello.py
@@ -0,0 +1,10 @@
+# Hello World od Bogusza
+# Zmęczony CEO Wszechświata pozdrawia
+
+def main():
+    print("Cześć, mów do sznura.")
+    print("Hello World! — Bogusz, CEO Wszechświat Sp. z o.o.")
+    print("*poprawia sznur*")
+
+if __name__ == "__main__":
+    main()
